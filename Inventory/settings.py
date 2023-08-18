0aebf3d6f78f9223256cd7120673277625a36c6d
OlukaGibson <gibsonoluka7@gmail.com>
2023-08-18 09:49:55 +0300

    Email Working

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -166,7 +166,7 @@ EMAIL_FROM = 'gibsonoluka7@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'gibsonoluka7@gmail.com'
-EMAIL_HOST_PASSWORD = 'ektlyhtczhronxwq'
+EMAIL_HOST_PASSWORD = 'mtaswnsbqrsrbawn'
 DEFAULT_FROM_EMAIL = 'gibsonoluka7@gmail.com'
 PASSWORD_RESET_TIMEOUT = 14400
 
