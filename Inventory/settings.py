70a4bac6463ae46ab20b0b0e8b77dc1353759df9
OlukaGibson <gibsonoluka7@gmail.com>
2023-09-02 11:43:20 +0300

    Secrets resloved

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import dj_database_url
 import environ
 import os
@@ -28,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lzk=5&kl!knvu2i110x*k$_@m9x4b(op7go%6sdb9jodn*c#*h'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -93,7 +94,7 @@ WSGI_APPLICATION = 'Inventory.wsgi.application'
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME':  'AirQoHardWare',
 #         'USER': 'postgres',
-#         'PASSWORD': 'P@ss1234',
+#         'PASSWORD': '',
 #         'HOST': 'localhost'
 #     }
 # }
@@ -166,7 +167,7 @@ EMAIL_FROM = 'gibsonoluka7@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'gibsonoluka7@gmail.com'
-EMAIL_HOST_PASSWORD = 'mtaswnsbqrsrbawn'
+EMAIL_HOST_PASSWORD =config("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = 'gibsonoluka7@gmail.com'
 PASSWORD_RESET_TIMEOUT = 14400
 
